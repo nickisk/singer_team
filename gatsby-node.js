5b9bdcc25b86b023669e4fa6030eb5a10545838e
shivam_iskpro <shivam.sharma@iskpro.com>
2020-02-28 16:55:10 +0530

    worked in config file

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,44 +2,49 @@ const Promise = require('bluebird')
 const path = require('path')
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+	const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/page.js')
-    resolve(
-      graphql(
-        `
+	return new Promise((resolve, reject) => {
+		const blogPost = path.resolve('./src/templates/page.js')
+		resolve(
+			graphql(
+				`
         {
             allContentfulNavigation {
               edges {
                 node {
                   title
-                  url
+				  url
+				  page {
+                    id
+                  }
                 }
               }
             }
           }
           
           `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
+			).then(result => {
+				if (result.errors) {
+					console.log(result.errors)
+					reject(result.errors)
+				}
 
-        console.log(result.data.allContentfulNavigation.edges)
+				console.log(result.data.allContentfulNavigation.edges)
 
-        const pages = result.data.allContentfulNavigation.edges
-        pages.forEach((page, index) => {
-          createPage({
-            path: `${page.node.url}/`,
-            component: blogPost,
-            context: {
-              slug: page.node.url
-            },
-          })
-        })
-      })
-    )
-  })
+				const pages = result.data.allContentfulNavigation.edges
+				pages.forEach((page, index) => {
+					if (page.node.page != null) {
+						createPage({
+							path: `${page.node.url}/`,
+							component: blogPost,
+							context: {
+								slug: page.node.url
+							},
+						})
+					}
+				})
+			})
+		)
+	})
 }
